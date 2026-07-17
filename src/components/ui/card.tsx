23063020ea8bd0406874c74da370ac55d4bf16f3
vicmat04 <vicmat04@gmail.com>
2026-07-17 16:00:35 -0500

    fix: make children prop optional in Card component to fix build error

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   className?: string;
   hoverEffect?: boolean;
 }
